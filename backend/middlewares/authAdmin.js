fb34eea59d98b09908eb505e1934dcc1a9161ebd
Radhika Singh <radhika_singh@lineaje.com>
2026-07-29 21:26:37 -0700

    Revert "LVE-7521-104601309 fix"
    This reverts commit 142ec8d2b40900ade654ffa8572299c749397217.

--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -8,7 +8,7 @@ const authAdmin = async (req, res, next) => {
             return res.json({ success: false, message: 'Not Authorized Login Again' })
         }
         const token_decode = jwt.verify(atoken, process.env.JWT_SECRET)
-        if (!token_decode || token_decode.role !== "admin")  {
+        if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
             return res.json({ success: false, message: 'Not Authorized Login Again' })
         }
         next()
